261cefd4665e6fbdde0a4fe57805564870c87c5f
dhheck <dheck@chicagobooth.edu>
2015-11-12 13:22:55 -0600

    DHH MSM Progress

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :director_id, :presence => true
+  validates :title, :presence => true, :uniqueness => :true
+  validates :year, :greater_than => 1870, :less_than => 2050
 end
